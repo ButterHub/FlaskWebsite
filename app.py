5a8eff02e57e51bd1feab4177991f526310bfa79
Adrian <24711048+ButterHub@users.noreply.github.com>
2018-11-10 22:07:27 +0000

    Written in IntelliJ idea

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+bob = Flask(__name__)
+
+@bob.route('/')
+def slash():
+    return render_template('index.html')
+
+@bob.route('/cv')
+def cv():
+    return render_template('cv.html')
+
+@bob.route('/engineering')
+def engineering():
+    return render_template('engineering.html')
+
+@bob.route('/research')
+def research():
+    return render_template('research.html')
+
+@bob.route('/photography')
+def photography():
+    return render_template('photography.html')
+
+# @bob.route('/lay')
+# def layout():
+#     return render_template('layout.html')
+
+if __name__ == "__main__":
+    bob.run(debug=True)
